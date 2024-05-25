2547de4cd885fdae99c9720ed202eedb1bba1406
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-05-25 13:07:51 +0700

    use middleware validate in register

--- src/routers/AuthRoutes.ts
+++ src/routers/AuthRoutes.ts
@@ -1,4 +1,5 @@
 import BaseRoutes from "./BaseRoutes"
+import validate from "../middlewares/AuthValidator";
 
 
 // Controllers
@@ -6,7 +7,7 @@ import AuthController from "../controllers/AuthController";
 
 class AuthRoutes extends BaseRoutes{
     public routes(): void{
-        this.router.post("/register", AuthController.register);
+        this.router.post("/register", validate, AuthController.register);
         this.router.post("/login", AuthController.login);
     }
 }
