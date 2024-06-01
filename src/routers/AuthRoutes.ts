2b425fad0f2238e8f6d7cd8ef548691019028ddc
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-06-01 22:30:55 +0700

    route:new route for profile

--- src/routers/AuthRoutes.ts
+++ src/routers/AuthRoutes.ts
@@ -1,5 +1,6 @@
 import BaseRoutes from "./BaseRoutes"
 import validate from "../middlewares/AuthValidator";
+import { auth } from "../middlewares/AuthMiddleware";
 
 
 // Controllers
@@ -8,7 +9,8 @@ import AuthController from "../controllers/AuthController";
 class AuthRoutes extends BaseRoutes{
     public routes(): void{
         this.router.post("/register", validate, AuthController.register);
-        this.router.post("/login", AuthController.login);
+        this.router.post("/login", validate, AuthController.login);
+        this.router.get("/profile", auth, AuthController.profile);
     }
 }
 
