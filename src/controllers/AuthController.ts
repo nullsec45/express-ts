481c0b8b3c78541c78970dbc7a6a2ccfd69ee475
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2024-06-01 22:16:48 +0700

    auth: login and generated token

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,19 +1,50 @@
 import {Request, Response} from "express";
-import PasswordHash from "../utils/PasswordHash";
+import Authenctication from "../utils/Authentication";
+import { compare } from "bcrypt";
 const db=require("../db/models/");
 
 class AuthController {
     register=async (req: Request, res: Response): Promise<Response> => {
         let {username, password}=req.body;
-        const hashedPassword:string=await PasswordHash.hash(password);
+        const hashedPassword:string=await Authenctication.passwordHash(password);
 
         await db.user.create({username, password:hashedPassword});
 
         return res.send("Registration Success");
     }
 
-    login(req: Request, res: Response): Response {
-        return res.send("");
+    login=async (req: Request, res: Response): Promise<Response> => {
+            // cari data user by username
+        let {username, password}=req.body;
+
+        const user=await db.user.findOne({
+            where:{username}
+        });
+
+        if(user){
+            let compare= await Authenctication.passwordCompare(password, user.password);
+
+            // check passwor
+
+            if(compare){
+                // generate token
+
+                let token=Authenctication.generateToken(user.id, username);
+                return res.send({
+                    token
+                });
+            }
+
+            return res.send("Auth Failed");
+        }
+        
+
+        return res.send("user not found");
+        
+    }
+
+    profile=(req:Request, res:Response) : Response => {
+        return res.send("Ini adalah profile, anda sudah login :).");
     }
 }
 
